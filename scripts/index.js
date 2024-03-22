94092b3c542be711d685809c7fd5035720a6f6c9
Guilherme Machado <guilherme.machado@mblabs.com.br>
2024-03-22 20:59:50 -0300

    feat: add element creation using buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 function teste() {
     alert('teste')
 }
+
+
+
+function addNewElement(type) {
+    const editor = document.getElementById('editor')
+    const element = document.createElement(type)
+    element.innerText = 'Element'
+    editor.append(element)
+}
